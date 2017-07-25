ca60ee47c3f140e5eb39ab286e36fa4f705263f8
Tyler Calkins <atcalkins@outlook.com>
2017-07-25 08:50:13 -0400

    got seconds to work with new date difference, still need correct if statements for leap year and dst

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const chalk = require("chalk");
 const log = console.log;
 const timestamp = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 const numberOfYear = moment().format("DDD");
-const secondsDay = moment().format("s");
+const secondsDay = moment();
+const secondsMidnight = secondsDay.clone().startOf('day');
+const diffSeconds = secondsDay.diff(secondsMidnight, 'seconds');
 const dst = moment().isDST();
+const savings = "Is";
+  if (dst) {
+    savings === "Isn't"
+  };
+
+
 const leapYear = moment().isLeapYear();
 moment().format();
 
 log("It is " + chalk.blue.bold(timestamp));
 log("It is the " + chalk.magenta.bold(numberOfYear + "th") + " Day of the year.");
-log("It is " + chalk.cyan.bold(secondsDay) + " seconds into the day. ");
-log("It " + chalk.yellow.bold(dst) + " during Daylights Savings Time.");
+log("It is " + chalk.cyan.bold(diffSeconds) + " seconds into the day. ");
+
+log("It " + chalk.yellow.bold('savings') + " during Daylights Savings Time.");
+
+
 log("It " + chalk.red.bold(leapYear) + " not a leap year.");
