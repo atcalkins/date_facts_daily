0b12fcf57c2635daf7087ff54fbed585a9c63b8e
Tyler Calkins <atcalkins@outlook.com>
2017-07-25 08:51:34 -0400

    missed save

--- app.js
+++ app.js
@@ -7,20 +7,15 @@ const secondsDay = moment();
 const secondsMidnight = secondsDay.clone().startOf('day');
 const diffSeconds = secondsDay.diff(secondsMidnight, 'seconds');
 const dst = moment().isDST();
-const savings = "Is";
-  if (dst) {
-    savings === "Isn't"
-  };
+const savings = 
 
 
 const leapYear = moment().isLeapYear();
 moment().format();
+const
 
 log("It is " + chalk.blue.bold(timestamp));
 log("It is the " + chalk.magenta.bold(numberOfYear + "th") + " Day of the year.");
 log("It is " + chalk.cyan.bold(diffSeconds) + " seconds into the day. ");
-
 log("It " + chalk.yellow.bold('savings') + " during Daylights Savings Time.");
-
-
 log("It " + chalk.red.bold(leapYear) + " not a leap year.");
